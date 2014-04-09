9039e6595a664bcdca37f5f9ee470a394eb775d6
ruby -v <leo_mejia_p@hotmail.com>
2014-04-09 10:19:54 -0400

    subiendo cambios editar lista grupos

--- application/views/grupos/ver_lista_de_grupos_por_entrenador_view.php
+++ application/views/grupos/ver_lista_de_grupos_por_entrenador_view.php
@@ -48,21 +48,12 @@
 		              </tr>
 		            </thead>
 		            <tbody>
+		            <!--<td><a href="<?php # echo base_url(); ?>grupos/Grupo_controller/ver_grupo/">Ver</a></td> -->
 		              <?php  
 		                $cont = 1;
 		                foreach($entrenador['grupos'] as $itemGrupos){
 		                  echo "<tr>";
-                      
 		                    echo "<td>".$cont."</td>";
-
-		                    echo "<td>".$itemGrupos['nombreGrupo']."</td>";?>
-		                    <td><a href="<?php echo base_url(); ?>grupos/Grupo_controller/ver_grupo/">Ver</a></td>
-                   
-		                    <td><a href="">Editar</a></td>
-							<td><a href="">Eliminar</a></td>
-		                 
-                        <?php
-                        echo "</tr>";
 		                    echo "<td>".$itemGrupos['nombreGrupo']."</td>";
 		                    echo '<td><a href="">Ver</a></td>';
 		                    echo '<td><a href="'.base_url().'index.php/Grupo_controller/editar_grupo/'.$itemGrupos['idGrupo'].'">Editar</a></td>';
