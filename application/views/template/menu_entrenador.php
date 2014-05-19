781b9e0f5ffef39e76f62b10566b3d540e9c1477
ruby -v <leo_mejia_p@hotmail.com>
2014-05-19 10:32:35 -0400

    modificando navegacion de entrenador

--- application/views/template/menu_entrenador.php
+++ application/views/template/menu_entrenador.php
@@ -16,16 +16,16 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"> 
         <!--   Grupo  -->
-        <ul class="nav navbar-nav">
+        <!-- <ul class="nav navbar-nav">
 	        <li class="dropdown">
 	          <a href="#" class="dropdown-toggle" data-toggle="dropdown">GRUPOS <b class="caret"></b></a>
 	          <ul class="dropdown-menu">
-	            <li><a href="<?php echo base_url(); ?>index.php/Grupo_controller/ver_mis_grupo">Ver lista de grupos</a></li>
-	            <!--  <li class="divider"></li>
-	            <li><a href="#">Seleccionar Planilla</a></li>-->
+	             <li><a href="<?php #echo base_url(); ?>index.php/Grupo_controller/ver_mis_grupo">Ver lista de grupos</a></li>
+	              <li class="divider"></li>
+	            <li><a href="#">Seleccionar Planilla</a></li>
 	          </ul>        
 	        </li>
-      	</ul>       
+      	</ul> -->      
 
       	<!--   Planilla  -->
         <ul class="nav navbar-nav">
